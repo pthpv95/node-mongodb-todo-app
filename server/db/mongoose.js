e63e7cf6a4e0b19d725cd14a9bd2d5a721128ea3
Hien Pham <thehienpv@gmail.com>
2018-11-10 16:37:02 +0700

    set up for heroku with new addons

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,8 @@
 const { isProd } = require("../utils");
-const { PRODUCTION_DATABASE_URI } = require("../../config");
-
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-const url = isProd
-  ? PRODUCTION_DATABASE_URI
-  : "mongodb://localhost:27017/TodoApp";
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 
 mongoose.connect(
   url,
