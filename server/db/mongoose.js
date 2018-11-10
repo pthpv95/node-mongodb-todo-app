addba873e285a2feab3b8d75cf5571d015482ee3
Hien Pham <thehienpv@gmail.com>
2018-11-10 14:09:27 +0700

    add mongodb production url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,14 @@
+const { isProd } = require("../utils");
+
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
+const url = isProd
+  ? "todo-app-shard-00-00-obwkd.mongodb.net:27017"
+  : "mongodb://localhost:27017/TodoApp";
+
 mongoose.connect(
-  "mongodb://localhost:27017/TodoApp",
+  url,
   { useNewUrlParser: true }
 );
 
