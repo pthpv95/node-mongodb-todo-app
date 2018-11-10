055050e925a1c6bc7b73712aed0a41efff63c526
Hien Pham <thehienpv@gmail.com>
2018-11-10 15:26:53 +0700

    update connection string to match with current mongodb version

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   PRODUCTION_DATABASE_URI:
-    "mongodb+srv://pthpv95:0985445290H@todo-app-obwkd.mongodb.net/test?retryWrites=true"
+    "mongodb://pthpv95:0985445290H@todo-app-shard-00-00-obwkd.mongodb.net:27017,todo-app-shard-00-01-obwkd.mongodb.net:27017,todo-app-shard-00-02-obwkd.mongodb.net:27017/test?ssl=true&replicaSet=todo-app-shard-0&authSource=admin&retryWrites=true"
 };
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,11 +4,10 @@ const { PRODUCTION_DATABASE_URI } = require("../../config");
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-// const url = !isProd
-//   ? PRODUCTION_DATABASE_URI
-//   : "mongodb://localhost:27017/TodoApp";
+const url = isProd
+  ? PRODUCTION_DATABASE_URI
+  : "mongodb://localhost:27017/TodoApp";
 
-const url = PRODUCTION_DATABASE_URI;
 mongoose.connect(
   url,
   { useNewUrlParser: true }
