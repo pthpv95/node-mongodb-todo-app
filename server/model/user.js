975a4e18c54852695ac4d59cc75c962d7f0f6f93
Hien Pham <thehienpv@gmail.com>
2018-11-12 21:20:00 +0700

    Add POST /users/login

--- server/model/user.js
+++ server/model/user.js
@@ -53,6 +53,28 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, success) => {
+        console.log(success);
+
+        if (success) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.methods.toJSON = function() {
   var user = this;
   return _.pick(user, ["_id", "email"]);
--- server/server.js
+++ server/server.js
@@ -136,6 +136,20 @@ app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post("/users/login", (req, res) => {
+  const { email, password } = req.body;
+
+  User.findByCredentials(email, password)
+    .then(user => {
+      return user.generateAuthToken().then(token => {
+        res.header("x-auth", token).send(user);
+      });
+    })
+    .catch(e => {
+      res.send(400).send();
+    });
+});
+
 app.listen(port, () => {
   console.log("Starting on port" + port);
 });
